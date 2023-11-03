c32e631a2ae347796f57dbc0dea86792baf66caf
Jereissate <rafaeljereissate@hotmail.com>
2023-11-03 11:42:30 -0300

    alterações no tamplete string do butão e da imagem

--- src/main.js
+++ src/main.js
@@ -10,19 +10,43 @@ const app = $('#app');
 
 app.innerHTML = `
   <div>
-    ${logo({link: "https://vitejs.dev", imgSrc: viteLogo, descricao: "Vite logo"})}
-    ${logo({link: "https://www.typescriptlang.org/", imgSrc: typescriptLogo, descricao: "TypeScript logo"})}
+    
+    ${logo("https://vitejs.dev",viteLogo,"Vite logo")}
+    ${logo("https://www.typescriptlang.org/",typescriptLogo,"TypeScript logo")}
+
 
     <h1>Vite + TypeScript</h1>
-    
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
     ${counter()}
     ${counter()}
-    
+
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
-  </div>
+    </div>
 `
+const button = $('#counter')
+setupCounter(button);
+function counter() {
+  return `
+  <div class="card">
+  <button id="counter" type="button"></button>
+  </div>
+`;
+}
 
-setupCounter($('#counter'));
+/**
+ * @param {string} link
+ * @param {string} imagem
+ * @param {string} comentario
+ */
+function logo(link, imagem, comentario) {
+  return `
+  <a href="${link}">
+  <img src="${imagem}" class="logo" alt= "${comentario}" />
+  </a>
+  `
+}
 
