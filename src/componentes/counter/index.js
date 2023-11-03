384ed362510aa07d682ef22ad9d196530c9d4c5e
Mansueto Paiva <mansuetoonofre@gmail.com>
2023-11-03 12:27:48 -0300

    fiz alteracoes

---
+++ src/componentes/counter/index.js
@@ -0,0 +1,6 @@
+export function counter() {
+    return `<div class="card">
+        <button id="counter" type="button"></button>
+      </div>`
+  }
+  
---
+++ src/componentes/logo/index.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} link
+ * @param {string} img
+ * @param {string} descricao
+ */
+export function logo(link, img, descricao) {
+    return `<a href="${link}" target="_blank">
+    <img src="${img}" class="logo" alt="${descricao}" />
+  </a>`
+  }
