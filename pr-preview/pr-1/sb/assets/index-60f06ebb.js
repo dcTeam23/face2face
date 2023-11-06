ef818d444db43459672e0b8fe17c06bb2a924cbf
AbraaoAlves <AbraaoAlves@users.noreply.github.com>
2023-11-06 09:39:20 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/sb/assets/index-60f06ebb.js
@@ -0,0 +1,2 @@
+import{M as n,a as M,u as D,w as X}from"./index-948fc7e0.js";import"./index-9cfb5d77.js";import"./iframe-52904512.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-de833af9.js";import"./index-d37d4223.js";import"./index-d2c24ff6.js";import"./index-356e4a49.js";export{n as MDXContext,M as MDXProvider,D as useMDXComponents,X as withMDXComponents};
+//# sourceMappingURL=index-60f06ebb.js.map
