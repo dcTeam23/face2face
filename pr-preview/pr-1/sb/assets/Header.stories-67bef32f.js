b90ac804ca4b6c8430a05a2f2a0d39d58d331820
AbraaoAlves <abraao.teodosio@gmail.com>
2023-11-06 09:42:51 -0300

    remove sb files

--- pr-preview/pr-1/sb/assets/Header.stories-67bef32f.js
+++
@@ -1,8 +0,0 @@
-import{c as u}from"./Header-7d4ec7f6.js";import"./Button-c7137a63.js";const p={title:"Example/Header",tags:["autodocs"],render:g=>u(g),parameters:{layout:"fullscreen"},argTypes:{onLogin:{action:"onLogin"},onLogout:{action:"onLogout"},onCreateAccount:{action:"onCreateAccount"}}},e={args:{user:{name:"Jane Doe"}}},o={};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var n,s,c;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(s=o.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const i=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,i as __namedExportsOrder,p as default};
-//# sourceMappingURL=Header.stories-67bef32f.js.map
