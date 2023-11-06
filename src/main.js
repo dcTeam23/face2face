ab3c334a9da41e672535ae8265ad2d57f1e10d1c
AbraaoAlves <abraao.teodosio@gmail.com>
2023-11-05 21:08:05 -0300

    chore: add storybook logo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
+import storybookLogo from '/storybook.svg'
 import { setupCounter } from './counter'
 import { $ } from './select';
 import { counter } from './components/counter';
@@ -12,10 +13,10 @@ app.innerHTML = `
   <div>
     ${logo({link: "https://vitejs.dev", imgSrc: viteLogo, descricao: "Vite logo"})}
     ${logo({link: "https://www.typescriptlang.org/", imgSrc: typescriptLogo, descricao: "TypeScript logo"})}
+    ${logo({link: "https://storybookjs.org/", imgSrc: storybookLogo, descricao: "Storybook logo"})}
 
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + TypeScript + Storybook</h1>
     
-    ${counter()}
     ${counter()}
     
     <p class="read-the-docs">
