166cf0348bf9d2b60959e05caf6db6005c63e66e
João Pedro Cunha <jp5183179@gmail.com>
2023-11-10 15:33:23 +0000

    my first commit

---
+++ src/components/card/card.stories.ts
@@ -0,0 +1,30 @@
+import type { StoryObj, Meta } from '@storybook/html';
+
+import { CardProps, card } from './card';
+
+// More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
+const meta = {
+  title: 'App/Card',
+  tags: ['autodocs'],
+  render: (args) => {
+    return card(args);
+  },
+  argTypes: {
+
+    nome: {description: 'nome do profile'},
+    cargo: {description: 'cargo do profile'},
+    img: {description: 'endereço para a imgaem do logo'},
+  }
+} satisfies Meta<CardProps>;
+
+export default meta;
+type Story = StoryObj;
+
+// More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
+export const Primary: Story = {
+  args: {
+   nome: 'João Pedro',
+   cargo: 'Student Full Stack Developer',
+   img: 'https://avatars.githubusercontent.com/u/132667649?v=4',
+  }
+};
