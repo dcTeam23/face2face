787ba73ed92fe51c8f6108910910e1bd52962ed5
AbraaoAlves <AbraaoAlves@users.noreply.github.com>
2023-11-11 00:22:31 +0000

    Deploy preview for PR 1 🛫

---
+++ pr-preview/pr-1/assets/counter.stories-b32694ea.js
@@ -0,0 +1,10 @@
+function a(){return`
+    <div class="card">
+      <button id="counter" type="button">
+        count is 0
+      </button>
+    </div>
+  `}const s={title:"App/Counter",tags:["autodocs"],parameters:{layout:"fullscreen"},render:()=>a()},r={args:{}};var t,e,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {}
+}`,...(o=(e=r.parameters)==null?void 0:e.docs)==null?void 0:o.source}}};const n=["Primary"];export{r as Primary,n as __namedExportsOrder,s as default};
+//# sourceMappingURL=counter.stories-b32694ea.js.map
