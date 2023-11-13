020269d98f740de3883565eb8e65897c1f81dc45
Janaina Tannus <61756665+jmtannus@users.noreply.github.com>
2023-11-13 12:10:12 -0300

    criando classe avatar-container em index.js

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -4,11 +4,14 @@ import './avatar.css'
  * @typedef {Object} AvatarProps
  * @property {string} AvatarProps.imgSrc - some description here
  * @property {string} AvatarProps.descricao - some description here
- * 
+ * @property {string} avatarProps.link - some description here
+ *
  * @param {AvatarProps} props
  */
-export function avatar({imgSrc, descricao}) {
+export function avatar({imgSrc, descricao, link}) {
   return `
-    <img src="${imgSrc}" class="avatar" alt="${descricao}" />
+    <a class="avatar-container" href="${link}" target="_blank">
+      <img src="${imgSrc}" class="avatar" alt="${descricao}" />
+    </a>
   `
 }
