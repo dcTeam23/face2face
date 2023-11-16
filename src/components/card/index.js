03296aa6ffb8bd42c53ba72e466a610e8b85eacd
Janaina Tannus <61756665+jmtannus@users.noreply.github.com>
2023-11-16 15:31:46 -0300

    acrescentando caracteristicas na função card em index.js

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,26 +2,29 @@ import { avatar } from "../avatar";
 import './card.css'
 
 /**
- * @typedef {*} CardProps //*troca por Object?*//
- * @property {string} CardProps.nome
- * @property {string} CardProps.cargo
- * @property {string} CardProps.img
- * @param {CardProps} param0 
- * 
+ * @typedef { Object } CardProps
+ * @property { string } CardProps.nome
+ * @property { string } CardProps.cargo
+ * @property { string } CardProps.img 
+ * @property {string} CardProps.idade
+ * @property {Selection} CardProps.cor
+ * @property {boolean} CardProps.blur
+ * @param { CardProps } props
  */
 
-export function card(props) {
-    const {nome, cargo, img} = props;
-    return `<div class="card">
-        <div class="header">
-            ${avatar({
-                imgSrc: img, 
-                descricao:`profile picture from ${nome
-            }`)}
+export function card({ nome, cargo, img, idade, cor, blur }) {
+    return `
+  <div class="card ${cor}">
+  <div class="card-container">
+      ${avatar({ imgSrc: img, descricao: "", link: "" })}
+      <div class="footer ${blur}">
+      <div class="info">
+          <p>${nome}</p>
+          <span>${cargo}, ${idade}y </span>
         </div>
-        <div class="body">
-        <p class="nome">${nome}</p>
-        <p class="cargo">${cargo}</p>
-        </div>
-    </div>`;    
-  }
+          <div class="button-white"></div>
+      </div>
+  </div>
+</div>
+    `;
+}
