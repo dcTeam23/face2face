43bd430875d24d1275aa3d6b973f88679f21b156
Yan Felipe <yanfelipe9@hotmail.com>
2023-11-17 20:41:07 +0000

    creating square avatar

--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -2,14 +2,21 @@ import "./avatar.css";
 
 /**
  * @typedef {Object} AvatarProps
- * @property {string} AvatarProps.imgSrc - some description here
- * @property {string} AvatarProps.descricao - some description here
+ * @property {string} AvatarProps.imgSrc
+ * @property {string} AvatarProps.descricao 
+ * @property {boolean} AvatarProps.square
  * @param {AvatarProps} props
  */
-export function avatar({ imgSrc, descricao }) {
-  return `
-  <div class="container">
-      <img src="${imgSrc}" class="avatar" alt="${descricao}" />
-      </div>
-  `;
+export function avatar({ imgSrc, descricao, square }) {
+
+  if (square) {
+    return `
+    <img src="${imgSrc}" class="square" alt="${descricao}" />
+`;
+  } else {
+    return `
+    <img src="${imgSrc}" alt="${descricao}" />
+`;
+  }
+
 }
