4e6b51f913689f978ffaa79a6649b33383c49c2f
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-17 17:39:19 -0300

    Update avatar.stories.ts

--- src/components/avatar/avatar.stories.ts
+++ src/components/avatar/avatar.stories.ts
@@ -1,18 +1,16 @@
 import type { StoryObj, Meta } from '@storybook/html';
-
-import { avatar } from './index';
-import type {AvatarProps} from './index';
+import { AvatarProps, avatar } from './index';
 
 // More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 const meta = {
-  title: 'App/Avatar',
+  title: 'App/avatar',
   tags: ['autodocs'],
   render: (args) => {
     return avatar(args);
   },
   argTypes: {
-    imgSrc: {description: 'endereço para a imagem do avatar'},
-    descricao: {description: 'descrição da imagem do avatar'}
+    imgSrc: {description: 'endereço para a imagem do logo'},
+    descricao: {description: 'descrição da imagem do logo'}
   }
 } satisfies Meta<AvatarProps>;
 
@@ -21,8 +19,8 @@ type Story = StoryObj;
 
 // More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
 export const Primary: Story = {
-  args: {
-    imgSrc: 'https://avatars.githubusercontent.com/u/103774096?s=400&u=4fe9f493354559b971033b9dc7852c7f966f1264&v=4', 
-    descricao: 'Foto do avatar '
+  args: { 
+    imgSrc: 'https://avatars.githubusercontent.com/u/103774096?v=4', 
+    descricao: 'Octocat from Github '
   }
 };
