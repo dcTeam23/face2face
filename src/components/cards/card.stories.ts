ca45a0ae472024663e32bfcb024ff2fe7b9d33a8
AbraaoAlves <abraao.teodosio@gmail.com>
2023-11-24 10:15:50 +0000

    fix: duplicate card story title

--- src/components/cards/card.stories.ts
+++ src/components/cards/card.stories.ts
@@ -3,7 +3,7 @@ import type { CardProps } from './index';
 import { card } from './index';
 // More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 const meta = {
-  title: 'App/card',
+  title: 'App/cards',
   tags: ['autodocs'],
   render: (args) => {
     return card(args);
