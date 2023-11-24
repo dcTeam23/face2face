796116727413b18b1d9bada23e498d31aa4cba4b
João Vitor Rodrigues Farias <84153149+jv-farias@users.noreply.github.com>
2023-11-24 08:43:40 -0300

    Ideia de Card Expandido  (#28)
    * upload icons
    
    * fix: conflito resolvido
    
    * componente icones dos hobbys concluido
    
    * componente card concluído
    
    * fix: remove old component card file
    
    * remove icons image
    
    * opção de formato do avatar - square or circle
    
    * estrutura dos cards finalizadas - card e card-expanded
    
    * estilizacao concluida - light e dark - card e card-expanded
    
    * storybook concluido
    
    * remove HobbyIcon component

--- src/components/avatar/avatar.stories.ts
+++ src/components/avatar/avatar.stories.ts
@@ -3,14 +3,19 @@ import { AvatarProps, avatar } from './index';
 
 // More on how to set up stories at: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 const meta = {
-  title: 'App/avatar',
+  title: 'App/Avatar',
   tags: ['autodocs'],
   render: (args) => {
     return avatar(args);
   },
   argTypes: {
-    imgSrc: {description: 'endereço para a imgaem do logo'},
-    descricao: {description: 'descrição da imagem do logo'}
+    imgSrc: {description: 'Endereço (URL) da Imagem do Avatar'},
+    descricao: {description: 'Descrição da Imagem do Avatar'},
+    format : {
+      description: 'Formato do Avatar',
+      control: { type: 'radio' },
+      options: ['circle', 'square'],
+    }
   }
 } satisfies Meta<AvatarProps>;
 
@@ -18,12 +23,19 @@ export default meta;
 type Story = StoryObj;
 
 // More on writing stories with args: https://storybook.js.org/docs/html/writing-stories/args
-export const Primary: Story = {
+export const Circle: Story = {
   args: { 
     imgSrc: 'https://s2-g1.glbimg.com/gZrnzQkY70EZ2ZP0zwB689bUAak=/0x0:5184x3456/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/9/e/EYKMBXR72tNdkUAgmZzQ/azul-caneta.jpg', 
-    descricao: 'Octocat from Github '
+    descricao: 'Octocat from Github',
+    format: 'circle'
   }
 };
 
-
+export const Square: Story = {
+  args: { 
+    imgSrc: 'https://s2-g1.glbimg.com/gZrnzQkY70EZ2ZP0zwB689bUAak=/0x0:5184x3456/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/9/e/EYKMBXR72tNdkUAgmZzQ/azul-caneta.jpg', 
+    descricao: 'Octocat from Github',
+    format: 'square'
+  }
+};
 
--- src/components/avatar/index.js
+++ src/components/avatar/index.js
@@ -4,12 +4,21 @@ import "./avatar.css";
  * @typedef {Object} AvatarProps
  * @property {string} AvatarProps.imgSrc - some description here
  * @property {string} AvatarProps.descricao - some description here
+ * @property { 'square' | 'circle' } AvatarProps.format - Formato do Avatar (Circular ou Quadrado)
  * @param {AvatarProps} props
  */
-export function avatar({ imgSrc, descricao }) {
-  return `
-  <div class="container">
-      <img src="${imgSrc}" class="avatar" alt="${descricao}" />
-      </div>
-  `;
+export function avatar({ imgSrc, descricao, format }) {
+  if (format == "square") {
+    return `
+    <div class="container">
+      <img src="${imgSrc}" class="avatar square" alt="${descricao}" />
+    </div>
+    `;
+  } else {
+    return `
+    <div class="container">
+      <img src="${imgSrc}" class="avatar circle" alt="${descricao}" />
+    </div>
+    `;
+  }
 }
