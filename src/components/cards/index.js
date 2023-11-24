a3d0a246a146604f88b43e0a300e7f919132c5e8
João Vitor R. Farias <jvmnc232@gmail.com>
2023-11-24 03:09:12 +0000

    estrutura dos cards finalizadas - card e card-expanded

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,27 +1,41 @@
 import { avatar } from "../avatar";
-import "./card.css"
+import "./card.css";
 /**
  * @typedef { Object } CardProps
  * @property { string } CardProps.nome
  * @property { string } CardProps.cargo
- * @property { string } CardProps.img 
+ * @property { string } CardProps.img
+ * @property { string } CardProps.mode
+ * @property { boolean } CardProps.expanded
+ * @property { string } CardProps.bannerColor
  * @param { CardProps } props
  */
 
-export function card({ nome, cargo, img }) {
-  return `
-  <div class="card">
-        <div class="container">
-            <div class="header">
-                ${avatar({ imgSrc: img, descricao: "" })}
-            </div>
-
-            <div class="body">
-                <p class="nome"> ${nome}</p>
-                <p class="cargo"> ${cargo}</p>
-            </div>
-
+export function card({ nome, cargo, img, expanded, bannerColor, mode }) {
+  if (expanded == true) {
+    return `
+      <div class="card-expanded ${mode}" style="border: 5px solid ${bannerColor}">
+        <div class="body-expanded" >
+        ${avatar({ imgSrc: img, descricao: "", format: "circle" })}
+        <div class="info-profile">
+          <p class="nome"> ${nome}</p>
+          <p class="cargo"> ${cargo}</p>
+          </div>
         </div>
+      </div>
+      `;
+  } else {
+    return `
+      <div class="card ${mode}">
+        <div class="header" style="background-color: ${bannerColor}">
+          ${avatar({ imgSrc: img, descricao: "", format: "circle" })}
         </div>
-    `;
+    
+        <div class="body">
+          <p class="nome"> ${nome}</p>
+          <p class="cargo"> ${cargo}</p>
+        </div>
+      </div>
+      `;
+  }
 }
