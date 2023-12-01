f755dd7964d5c7292bfe6cd3967a7637e8c9c4a9
Mansueto Paiva <mansuetoonofre@gmail.com>
2023-12-01 10:49:54 -0300

    migraçao para react

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig} from 'vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: (process.env.BASE_URL ?? '/')
-});
+  plugins: [react()],
+})
